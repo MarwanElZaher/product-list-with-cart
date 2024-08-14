c8798dee40bbbab4b9a41f7d96b5c6bbf4257e38
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-08-14 19:46:18 +0300

    feat: created cart static & its styles

--- src/components/Cart/index.jsx
+++ src/components/Cart/index.jsx
@@ -1,11 +1,43 @@
 import React from "react";
 import './style.css'
+import ReusableButton from "../ReusableButton";
+import { ReactSVG } from "react-svg";
 
 const Cart = () => {
     return (
-        <div>
+        <div className="cart-main-container">
+        <div className="cart-header">
+            <p>{`Your Cart (${0})`}</p>
+            </div>
+            {!true ?
+             <>
+                <div className="cart-body">
+                    <div className="cart-total">
+                        <div className="cart-total-label">
+                            Order Total
+                        </div>
+                        <div className="cart-total-value">
+                            $0
+                        </div>
+                    </div>
+                </div>
+            <div className="cart-footer">
+                <div className="delivery-slogan">
+                    <ReactSVG src='/assets/images/icon-carbon-neutral.svg' alt="icon-carbon-neutral-icon" />
+                    <p className="delivery-description">
+                        This is a <span>carbon-neutral</span> delivery
+                    </p>
+                </div>
+                <ReusableButton label="Confirm Order" className="confirm-order-button"/>
+                </div>
+                </> :
+                <div className="empty-cart">
+                    <ReactSVG src="/assets/images/illustration-empty-cart.svg" alt="illustration-empty-cart" />
+                    <p className="empty-cart-msg">Your added items would appear here</p>
+                </div>
+            }
 
-        </div>
+            </div>
     )
 }
 export default Cart
