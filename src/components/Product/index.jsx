7ce4710ea5d29ee491ce9bc565dc50108c27cd2e
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-08-14 20:58:45 +0300

    style: changed styles of the product buttons and its logic & now on add to cart the product is added to item inside the store

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import './style.css'
 import ReusableButton from "../ReusableButton";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/actions/cartActions";
+import Counter from "../Counter";
 
-const Product = ({image, name, category, price}) => {
+const Product = ({ image, name, category, price }) => {
+    const dispatch = useDispatch();
+
+    const handleAddingProductToCart = () => {
+        const product = {
+            product: name,
+            quantity: 1,
+        };
+        dispatch(addToCart(product))
+    }
+
     return (
     <>
         <div className="product-head">
@@ -11,18 +24,21 @@ const Product = ({image, name, category, price}) => {
                 <source media="(min-width: 768px)" srcSet={image.tablet} />
                 <source media="(min-width: 480px)" srcSet={image.mobile} />
                 <img src={image.thumbnail} alt={name} className="product-image" />
-            </picture>
-                <ReusableButton
+                </picture>
+                {/* after adding product to card the addtocartbutton to be hidden then -> counter to be appeared always check that the product is not in redux*/}
+                {true ?
+                    <ReusableButton
                     iconKey="/assets/images/icon-add-to-cart.svg"
-                    onClick={() => { }}
+                    onClick={handleAddingProductToCart }
                     label="Add to Cart"
                     className="add-to-cart-button"
-                />    
+                />
+                    : <Counter productName={name}/>}
         </div>
         <div className="product-body">
             <span className="product-category">{category}</span>
             <span className="product-name">{name}</span>
-            <span className="product-price">{ `$ ${price}`}</span>
+            <span className="product-price">{`$ ${price}`}</span>
     
             </div>
         </>
