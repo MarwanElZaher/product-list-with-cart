2614e4203ef55b0704fd7fcc5756da86b1001d8e
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-08-16 23:45:06 +0300

    style: products styling and structure as design

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -1,39 +1,35 @@
 import React from "react";
 import './style.css'
 import ReusableButton from "../ReusableButton";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../../redux/actions/cartActions";
+import { useDispatch, useSelector } from "react-redux";
+import { incrementItem } from "../../redux/actions/cartActions";
 import Counter from "../Counter";
 
 const Product = ({ image, name, category, price }) => {
     const dispatch = useDispatch();
+    const cartItems = useSelector((state) => state.cart.items);
 
     const handleAddingProductToCart = () => {
-        const product = {
-            product: name,
-            quantity: 1,
-        };
-        dispatch(addToCart(product))
+        dispatch(incrementItem(name))
     }
 
     return (
     <>
-        <div className="product-head">
+        <div className={cartItems[name] ?"product-head active" : "product-head"}>
             <picture>
                 <source media="(min-width: 1024px)" srcSet={image.desktop} />
                 <source media="(min-width: 768px)" srcSet={image.tablet} />
                 <source media="(min-width: 480px)" srcSet={image.mobile} />
                 <img src={image.thumbnail} alt={name} className="product-image" />
                 </picture>
-                {/* after adding product to card the addtocartbutton to be hidden then -> counter to be appeared always check that the product is not in redux*/}
-                {true ?
+                {cartItems[name] ?
+                    <Counter productName={name}/> :
                     <ReusableButton
                     iconKey="/assets/images/icon-add-to-cart.svg"
                     onClick={handleAddingProductToCart }
                     label="Add to Cart"
                     className="add-to-cart-button"
-                />
-                    : <Counter productName={name}/>}
+                />}
         </div>
         <div className="product-body">
             <span className="product-category">{category}</span>
