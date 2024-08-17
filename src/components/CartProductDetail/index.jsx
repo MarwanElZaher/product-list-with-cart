1570570fb02c0b412a932ca8e25f8de81704f4a4
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-08-17 04:33:55 +0300

    feat: created utils file to hold helper functions

--- src/components/CartProductDetail/index.jsx
+++ src/components/CartProductDetail/index.jsx
@@ -3,8 +3,9 @@ import { useDispatch } from "react-redux";
 import ReusableButton from "../ReusableButton";
 import './style.css'
 import { removeFromCart } from "../../redux/actions/cartActions";
+import { getImageForItem } from "../../utils";
 
-const CartProductDetail = ({ priceMap, cartItems }) => {
+const CartProductDetail = ({ priceMap, cartItems, totalPrice, showProductImage, showPriceLarge }) => {
     const dispatch = useDispatch();
     const products = Object.entries(cartItems);
 
@@ -13,7 +14,7 @@ const CartProductDetail = ({ priceMap, cartItems }) => {
             {products.map(([productName, productQuantity]) => {
                 return (
                     <div key={productName} className="cart-product-item">
-                        <div className="cart-product-detail">
+                        {showPriceLarge &&<div className="cart-product-total-large"> {`$${(productQuantity * priceMap[productName]).toFixed(2)}`}</div>}                        <div className="cart-product-detail">
                             <div className="cart-product-name"> {productName}</div>
                                 <div className="cart-product-description"> 
                                     <div className="cart-product-quantity">{`${productQuantity}x`}</div>
@@ -21,12 +22,26 @@ const CartProductDetail = ({ priceMap, cartItems }) => {
                                     <div className="cart-product-total"> {`$${(productQuantity * priceMap[productName]).toFixed(2)}`}</div>
                             </div>
                         </div>
-                        <ReusableButton iconKey="/assets/images/icon-remove-item.svg" className="remove-product-cart" label={ `Remove ${productName}` } onClick={()=>dispatch(removeFromCart(productName))}/>
+                        {showProductImage ?
+                        <div className="product-img">
+                                <img src={getImageForItem(productName)} alt={ productName} />
+                        </div>
+                            :
+                        <ReusableButton iconKey="/assets/images/icon-remove-item.svg" className="remove-product-cart" label={`Remove ${productName}`} onClick={() => dispatch(removeFromCart(productName))} />
+                          
+                        }
                     </div>
                 )
 
             })}
-          
+           <div className="cart-total">
+                 <div className="cart-total-label">
+                       Order Total
+                  </div>
+                    <div className="cart-total-value">
+                          {`$ ${totalPrice}`}
+                   </div>
+                </div>
         </div>
     );
 };
